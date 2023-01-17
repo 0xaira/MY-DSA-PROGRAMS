351b366fa99989f370ca40576c647a28b1fe99e5
Arsenic <airajena07@gmail.com>
2023-01-17 10:18:24 +0530

    check

---
+++ Miscellaneous/check.cpp
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+  
+int main(void)
+{
+    int size = 6;
+    int val = 1;
+  
+    // array declaration
+    int arr[6];
+    int i;
+  
+    // initializing array elements
+    for (i = 0; i < size ; i++){
+        arr[i] = val;
+    }
+  
+     // print array
+     printf("The array is:");
+     for (i = 0; i < size ; i++){
+         printf("%d ", arr[i]);
+    }
+  
+    return 0;
+}
