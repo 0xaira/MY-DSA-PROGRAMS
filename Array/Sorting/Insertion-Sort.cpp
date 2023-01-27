1facd2d43f623fa019610ba9a83c2df6a0960fc3
Arsenic <airajena07@gmail.com>
2023-01-27 09:38:42 +0530

    Insertion Sort

---
+++ Array/Sorting/Insertion-Sort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+void insertionSort(int arr[], int size)
+{
+    for (int i = 0; i < size-1; i++)
+    {
+
+        for (int j = i + 1; j > 0; j--)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                swap(arr[j], arr[j - 1]);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+
+    int size;
+    cin >> size;
+    int *arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    insertionSort(arr, size);
+    printArray(arr, size);
+}
