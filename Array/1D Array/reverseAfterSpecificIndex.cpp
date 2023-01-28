a72adee31d2a5975ca809b3d0dab479679e5f118
Arsenic <airajena07@gmail.com>
2023-01-28 09:36:40 +0530

    Specific Reverse

---
+++ Array/1D Array/reverseAfterSpecificIndex.cpp
@@ -0,0 +1,35 @@
+#include <iostream>  
+using namespace std;  
+
+void reverseArray(int arr[], int size, int i){
+    
+    int j= size-1;
+    while(i<j){
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+}
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+
+    int size;
+    cin >> size;
+    int *arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    int i;
+    cin>>i;
+    reverseArray(arr,size,i);
+    printArray(arr,size);
+}
