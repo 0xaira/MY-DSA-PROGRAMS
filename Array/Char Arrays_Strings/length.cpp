164f8b3339b9ef1653425d37caa8408c60397cf3
Arsenic <airajena07@gmail.com>
2023-01-30 10:57:26 +0530

    reverse strings

--- Array/Char Arrays_Strings/length.cpp
+++ Array/Char Arrays_Strings/length.cpp
@@ -1,5 +1,18 @@
 #include <iostream>  
 using namespace std;  
+void reverse(char ch[],int n){
+    int s=0;
+    int e=n-1;
+    while(s<e){
+        swap(ch[s++],ch[e--]);
+    }
+
+}
+void printarr(char ch[],int n){
+    for(int i=0; i<n; i++){
+        cout<<ch[i];
+    }
+}
 int main(){
     char ch[100];
     cin>>ch;
@@ -7,5 +20,7 @@ int main(){
     for(int i=0; (ch[i] != '\0'); i++){
         count++;
     }
-    cout<<count;
+    int n= count;
+    reverse(ch,n);
+    printarr(ch,n);
 }
