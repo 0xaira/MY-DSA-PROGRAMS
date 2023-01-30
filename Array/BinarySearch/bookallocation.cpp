e80129539dc6287c251d0237fe27f54ad2f9ad18
Arsenic <airajena07@gmail.com>
2023-01-30 10:40:51 +0530

    length of char array

---
+++ Array/BinarySearch/bookallocation.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+// using namespace std;
+// void binarySearch(int arr[], int size, int key)
+// {
+    
+    
+// }
+// int main(){
+//      int size;
+//     cin >> size;
+//     int *arr = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     int start = 0;
+//     int end = size - 1;
+//     int mid = start + (end - start);
+//     while(start<end){
+//         if(key==arr[mid]){
+//             cout<<"element found at index"<<endl<<arr[mid-1];
+//         }
+//         if(key<arr[mid]){
+//             end=mid-1;
+//         }
+//         else{
+//             start=mid+1;
+//         }
+//         mid = start + (end - start);
+//     }
+// }
