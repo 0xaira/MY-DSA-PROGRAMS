70ebcb9e4a6606054b10968e491ed787b4e8b49e
Arsenic <airajena07@gmail.com>
2023-01-30 09:17:59 +0530

    Binary Serach

---
+++ Array/BinarySearch/binarysearch.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+void binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end - start);
+    while(start<end){
+        if(key==arr[mid]){
+            cout<<"element found at index"<<endl<<arr[mid-1];
+        }
+        if(key<arr[mid]){
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+        mid = start + (end - start);
+    }
+    
+}
+// void printArray(int arr[],int size){
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout<<arr[i];
+//     }
+// }
+int main()
+{
+
+    int size;
+    cin >> size;
+    int *arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    int key;
+    cin>>key;
+    binarySearch(arr, size, key);
+    // printArray(arr, size);
+}
