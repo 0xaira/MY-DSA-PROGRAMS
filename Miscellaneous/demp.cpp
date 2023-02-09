6a4b8d1f90607c8c378eaf030ce941c93bc0e06e
Arsenic <airajena07@gmail.com>
2023-02-09 08:31:52 +0530

    demp

--- Miscellaneous/demp.cpp
+++ Miscellaneous/demp.cpp
@@ -1,29 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define MOD 100000007
-#define SIZE 1e6+2 
-
-
 void result(){
-    ll num;
+    int num;
     cin>>num;
-    string str;
-    cin>>str;
-    map<char, ll>mp;
-    for(auto i:str) mp[i]++;
-    for(auto i:mp){
-        if(i.second>1){
-            cout<<num-2<<endl;
-            return;
+    int point=1;
+    vector<int>arr;
+    map<int, int>mp;
+    int notsec;
+    for(int i=0; i<num; i++){
+      cin>>notsec;
+      arr.push_back(notsec);
+    }
+    sort(arr.begin(),arr.end());
+    reverse(arr.begin(),arr.end());
+    for(int i=0; i<num;i++){
+        mp[arr[i]]++;
+    }
+    for(auto it:mp){
+        if(it.second%2){
+            point=0;
+            break;
         }
     }
-    cout<<-1<<endl;
+    if(point)
+    cout<<"Zenyk"<<endl;
+    else{
+        cout<<"Marichka"<<endl;
+    }
 }
 int main() {
 	
-	ll test;
+	int test;
 	cin>>test;
 	while(test--){
 	    result();
