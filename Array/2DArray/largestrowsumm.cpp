a77686c0528937fb45895db2e814c4c67e06e28a
Arsenic <airajena07@gmail.com>
2023-02-16 10:35:18 +0530

    print like a wave

---
+++ Array/2DArray/largestrowsumm.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+
+int rowSum(int arr[2][3],int row,int col){
+    vector<int> ans;
+    int maxi=0;
+    for (int row = 0; row < 2; row++)
+	{
+        int sum=0;
+        for(int col=0; col<3;col++){
+            sum+=arr[row][col];
+            ans.push_back(sum);
+              
+        }	
+	}
+    for(int i=0;i<ans.size();i++){
+        if(ans[i]>maxi){
+            maxi=ans[i];
+        }
+    }
+    cout<<maxi;
+    
+}
+int main(){
+    int arr[2][3];
+    
+	for (int row = 0; row < 2; row++)
+	{
+        for(int col=0; col<3;col++){
+            cin >> arr[row][col];
+        }
+		
+	}
+    rowSum(arr,2,3);
+}
---
+++ Array/2DArray/print-like-a-wave.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+
+vector<int> wave(int arr[2][3],int row,int col){
+    vector<int> ans;
+    for (int c = 0; c < col; c++)
+	{
+        if(c&1){
+           for(int r=row-1;r>=0;r--){
+               ans.push_back(arr[r][c]);
+           }
+           
+        }
+        else{
+             for(int r=0;r<row;r++){
+               ans.push_back(arr[r][c]);
+           }
+        }
+        	
+	}
+    for (int i = 0; i < ans.size(); i++)
+	{
+        cout<<ans[i]<<" ";
+		
+	}
+    cout<<endl;
+    
+}
+int main(){
+    int arr[2][3];
+    
+	for (int row = 0; row < 2; row++)
+	{
+        for(int col=0; col<3;col++){
+            cin >> arr[row][col];
+        }
+		
+	}
+    wave(arr,2,3);
+    
+}
