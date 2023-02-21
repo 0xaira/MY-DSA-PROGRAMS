2b7620dee872a7157fae14e2a1121132c6c7a838
Arsenic <airajena07@gmail.com>
2023-02-21 10:16:46 +0530

    C++ program to find the  sum of natural numbers up to n using recursion

--- Recursion/sumofn.cpp
+++ Recursion/sumofn.cpp
@@ -3,19 +3,32 @@
 // to n using recursion
 #include <iostream>
 using namespace std;
-
-int recurSum(int n)
-{
-    if (n <= 1)
-        return n;
-    int a= n + recurSum(n - 1);
-    return a;
+int sum(int n) {
+   if(n == 0)
+   return n;
+   else
+   return n + sum(n-1);
+}
+int main() {
+   int n;
+   cin>>n;
+   cout<<"Sum of first "<<n<<" natural numbers is "<<sum(n);
+   return 0;
 }
 
-int main()
-{
-    int n;
-    cin>>n;
-    cout << recurSum(n);
-    return 0;
-}
+// int recurSum(int n)
+// {
+//     if (n <= 1)
+//         return n;
+//     int a= n + recurSum(n - 1);
+//     return a;
+// }
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     cout << recurSum(n);
+//     return 0;
+// }
+
