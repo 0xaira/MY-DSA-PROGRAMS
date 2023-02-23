8ccc12db7c5dd4258a83a05dc156309d9660b7be
Arsenic <airajena07@gmail.com>
2023-02-23 10:33:15 +0530

    Swap Alternate

--- Array/1D Array/alternate-swaps.cpp
+++ Array/1D Array/alternate-swaps.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
 using namespace std;
-
-void swapAlternate(int *array, int size){
-    int i =0 ; 
-	while(i+1<size){
-    	// int temp=array[i];
-    	// array[i]=array[i-1];
-    	// array[i-1]=temp;
-        swap(array[i],array[i+1]);
-    	i = i + 2;
+void swapAlternate(int *array, int size)
+{
+	int i = 0;
+	while (i + 1 < size)
+	{
+		// int temp=array[i];
+		// array[i]=array[i-1];
+		// array[i-1]=temp;
+		swap(array[i], array[i + 1]);
+		i = i + 2;
 	}
-    
 }
-  
-
 int main()
 {
-	
-		int size;
-		cin >> size;
-		int *arr = new int[size];
-		for (int i = 0; i < size; i++)
-		{
-			cin >> arr[i];
-		}
-		swapAlternate(arr, size);
-		for (int i = 0; i < size; i++)
-		{
-			cout << arr[i] << " ";
-		}
-		cout << endl;
-		
-	}
 
+	int size;
+	cin >> size;
+	int *arr = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr[i];
+	}
+	swapAlternate(arr, size);
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
