f4b57e4df6dd65e6e8c3259e1ab774050846dfe6
Arsenic <airajena07@gmail.com>
2023-03-01 19:24:58 +0530

    book alocation

--- Array/BinarySearch/bookallocation.cpp
+++ Array/BinarySearch/bookallocation.cpp
@@ -1,31 +1,53 @@
-// #include <iostream>
-// using namespace std;
-// void binarySearch(int arr[], int size, int key)
-// {
+#include <iostream>
+using namespace std;
+bool isPossible(int arr[], int size, int stcount,int mid)
+{
+    int studentCount=1;
+    int pageSum=0;
+    for(int i=0;i<size;i++){
+        if(pageSum+arr[i]<=mid){
+            pageSum+=arr[i];
+        }
+        else{
+            studentCount++;
+            if(studentCount>stcount || arr[i]>mid){
+                return false;
+            }
+            pageSum+=arr[i];
+            
+        }
+    }
+    return true;
     
+}
+int main(){
+    int size;
+    cin >> size;
+    int *arr = new int[size];
+    int sum=0;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+     for (int i = 0; i < size; i++)
+    {
+        sum+= arr[i];
+    }
+    int start = 0;
+    int end = sum;
+    int mid = start + (end - start);
+    int ans=0;
+    int stcount;
+    cin>>stcount;
     
-// }
-// int main(){
-//      int size;
-//     cin >> size;
-//     int *arr = new int[size];
-//     for (int i = 0; i < size; i++)
-//     {
-//         cin >> arr[i];
-//     }
-//     int start = 0;
-//     int end = size - 1;
-//     int mid = start + (end - start);
-//     while(start<end){
-//         if(key==arr[mid]){
-//             cout<<"element found at index"<<endl<<arr[mid-1];
-//         }
-//         if(key<arr[mid]){
-//             end=mid-1;
-//         }
-//         else{
-//             start=mid+1;
-//         }
-//         mid = start + (end - start);
-//     }
-// }
+        if(isPossible(arr,size,stcount,mid)){
+            ans=mid;
+            end=mid-1;
+           
+        }
+        else{
+             start=mid+1;
+        }
+        mid = start + (end - start);
+    cout<<mid;
+}
