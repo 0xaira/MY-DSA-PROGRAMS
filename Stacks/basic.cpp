da35ac563abf8d6e41ebd134f95b7de20ad5ae46
Arsenic <airajena07@gmail.com>
2023-07-01 09:04:51 +0530

    Basic Stack

--- Stacks/basic.cpp
+++ Stacks/basic.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main()
+{
     stack<int> s;
     s.push(10);
     s.push(20);
     s.push(30);
-    cout<<s.top()<<endl;
+    cout << s.top() << endl;
     s.pop();
-    cout<<s.top()<<endl;
-    cout<<s.size()<<endl;
-    cout<<s.empty()<<endl;
+    cout << s.top() << endl;
+    cout << s.size() << endl;
+    cout << s.empty() << endl;
 }
