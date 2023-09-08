8a20cd58d8fcdce7961cd20b929b0f46806a741b
Arsenic <airajena07@gmail.com>
2023-09-08 09:47:58 +0530

    Maximum area of histogram

--- Stacks/max_area_histogram.cpp
+++ Stacks/max_area_histogram.cpp
@@ -1,59 +1,64 @@
 // Maximum Area in the Histogram
 #include <bits/stdc++.h>
 using namespace std;
+#include <Math.h>
 void solve (int arr[], int N)
 {
-    stack <pair<int, int>> s;
-    vector<int> v;
-    int psuedo_index = -1;
-    for (int i = 0; i < N; i++)
+    stack<pair<int,int>> s;
+    vector<int> left;
+    int pseudo_index = -1;
+    for(int i=0;i<N;i++)
     {
-        if (s.empty())
-            v.push_back(psuedo_index);
-        else if (s.top().first < arr[i])
-            v.push_back(s.top().second);
-        else if (s.top().first >= arr[i])
+        if(s.size()==0)
+            left.push_back(pseudo_index);
+        else if(s.size()>0 && s.top().first<arr[i])
+            left.push_back(s.top().second);
+        else if(s.size()>0 && s.top().first>=arr[i])
         {
-            while (!s.empty() && s.top().first >= arr[i])
+            while(s.size()>0 && s.top().first>=arr[i])
                 s.pop();
-            if (s.empty())
-                v.push_back(psuedo_index);
+            if(s.size()==0)
+                left.push_back(pseudo_index);
             else
-                v.push_back(s.top().second);
+                left.push_back(s.top().second);
         }
-        s.push({arr[i], i});
+        s.push({arr[i],i});
     }
-    while (!s.empty())
-        s.pop();
-    psuedo_index = N;
-    vector<int> v1;
-    for (int i = N - 1; i >= 0; i--)
-    {
-        if (s.empty())
-            v1.push_back(psuedo_index);
-        else if (s.top().first < arr[i])
-            v1.push_back(s.top().second);
-        else if (s.top().first >= arr[i])
-        {
-            while (!s.empty() && s.top().first >= arr[i])
-                s.pop();
-            if (s.empty())
-                v1.push_back(psuedo_index);
-            else
-                v1.push_back(s.top().second);
-        }
-        s.push({arr[i], i});
-    }
-    reverse(v1.begin(), v1.end());
-    int max_area = INT_MIN;
-    for (int i = 0; i < N; i++)
-    {
-        int area = (v1[i] - v[i] - 1) * arr[i];
-        max_area = max(max_area, area);
-    }
-    cout << max_area << endl;
 
-   
+    vector<int> right;
+    pseudo_index = N;
+    for(int i=N-1;i>=0;i--)
+    {
+        if(s.size()==0)
+            right.push_back(pseudo_index);
+        else if(s.size()>0 && s.top().first<arr[i])
+            right.push_back(s.top().second);
+        else if(s.size()>0 && s.top().first>=arr[i])
+        {
+            while(s.size()>0 && s.top().first>=arr[i])
+                s.pop();
+            if(s.size()==0)
+                right.push_back(pseudo_index);
+            else
+                right.push_back(s.top().second);
+        }
+        s.push({arr[i],i});
+    }
+    reverse(right.begin(),right.end());
+
+    int width[N];
+    for(int i=0;i<N;i++)
+        width[i] = right[i]-left[i]-1;
+    
+    int area[N];
+    for(int i=0;i<N;i++)
+        area[i] = width[i]*arr[i];
+
+    int max_area = INT_MIN;
+    for(int i=0;i<N;i++)
+        max_area = max(max_area,area[i]);
+
+    cout<<max_area<<endl;
 }
 
 int main()
